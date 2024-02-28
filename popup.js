4bc5c39d445e313745e06754b5492ec4a63d3449
Gabriela Sastre <159554389+Gabrielas82@users.noreply.github.com>
2024-02-28 11:56:03 -0300

    Delete popup.js

--- popup.js
+++
@@ -1,8 +0,0 @@
-import javax.swing.JOptionPane;
-
-public class PopupExample {
-    public static void main(String[] args) {
-        // Crear un pop-up con un mensaje
-        JOptionPane.showMessageDialog(null, "¡Hola! Este es un pop-up en Java.", "Pop-up", JOptionPane.INFORMATION_MESSAGE);
-    }
-}
