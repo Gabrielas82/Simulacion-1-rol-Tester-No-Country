44143db9d2317e6810e7f23a8c4325af4454406a
Gabriela Sastre <159554389+Gabrielas82@users.noreply.github.com>
2024-02-28 10:30:43 -0300

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class PopupExample {
+    public static void main(String[] args) {
+        // Crear un pop-up con un mensaje
+        JOptionPane.showMessageDialog(null, "¡Hola! Este es un pop-up en Java.", "Pop-up", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
